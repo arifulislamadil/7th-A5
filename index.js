2d4daa48b4f5d09ab958cbefc064d25ce073dcce
Ariful Islam Adil <adil.islam.arif@gmail.com>
2023-02-18 12:05:59 +0400

    added function

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-//triangle
-document.getElementById("triangle").addEventListener("click",function(){
-
+// common function for triangle and rectangle
+function triangleRectangle(title,base,height){
     // get data from html using id 
-    const triangleName = document.getElementById("triangle-name").innerText;
-    const triangleBaseInput = document.getElementById("tri-base");
-    const triangleBaseValue = triangleBaseInput.value;
-    const triangleHeightInput = document.getElementById("tri-height");
-    const triangleHeightValue = triangleHeightInput.value;
-    const totalArea = triangleBaseValue * triangleHeightValue;
+    const nameText = document.getElementById(title).innerText;
+    const inputBase = document.getElementById(base);
+    const baseValue = inputBase.value;
+    const inputHeight = document.getElementById(height);
+    const heightValue = inputHeight.value;
+    const totalArea = baseValue * heightValue;
 
     // get parent container 
     const container = document.getElementById("table-container");
@@ -15,13 +14,70 @@ document.getElementById("triangle").addEventListener("click",function(){
     const tr=document.createElement("tr");
     tr.innerHTML = `
     <td>${1}</td>
-    <td>${triangleName}</td>
+    <td>${nameText}</td>
     <td>${totalArea} cm <sup>2</sup></td>
     <button id="btn-cal">Convert to m <sup>2</sup></button>
     `;
 
     container.appendChild(tr)
-    triangleBaseInput.value = "";
-    triangleHeightInput.value = "";
+    inputBase.value = "";
+    inputHeight.value = "";
+}
+
+
+//triangle
+document.getElementById("triangle").addEventListener("click",function(){
+    triangleRectangle("triangle-name","tri-base","tri-height")
+    
 });
+
+//rectangle
+document.getElementById("rectangle").addEventListener("click",function(){
+    triangleRectangle("rectangle-name","rec-width","rec-height")
+    
+});
+
+// common function for parallelogram, Rhombus ,Pentagon, Ellipse 
+function commonFunc(number,title,base,height){
+    const nameText=document.getElementById(title).innerText;
+    const parBase = document.getElementById(base).innerText;
+    const parHeight = document.getElementById(height).innerText;
+    const area = number* parBase * parHeight;
+    console.log(1*parBase * parHeight);
+
+    // get parent container 
+    const container = document.getElementById("table-container");
+    // add to cart
+    const tr=document.createElement("tr");
+    tr.innerHTML = `
+    <td>${1}</td>
+    <td>${nameText}</td>
+    <td>${area.toFixed(2)} cm <sup>2</sup></td>
+    <button id="btn-cal">Convert to m <sup>2</sup></button>
+    `;
+
+    container.appendChild(tr)
+    
+}
+
+// parallelogram
+document.getElementById("parallelogram").addEventListener("click",function(){
+    commonFunc(1,"para-text","par-base","par-height");
+})
+//Rhombus
+document.getElementById("rhombus").addEventListener("click",function(){
+    commonFunc(0.5,"rhombus-text","rhom-d1","rhom-d2");
+})
+//Pentagon
+document.getElementById("pentagon").addEventListener("click",function(){
+    commonFunc(0.5,"pen-text","pen-p","pen-b");
+})
+
+//Ellipse
+document.getElementById("ellipse").addEventListener("click",function(){
+    commonFunc(3.14,"ellip-text","elli-a","elli-b");
+})
+
+
+
 
